c3332f6898420571db0b22f3f6121da5ec8bf8b9
Jorge Zavala <jorge.zavala@gmail.com>
2025-03-06 10:03:07 -0800

    first tested version in local form

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
-
 import requests
+from typing import Optional
+from argparse import RawTextHelpFormatter
 import streamlit as st
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
-BASE_API_URL = "https://api.langflow.astra.datastax.com"
-LANGFLOW_ID = "edca2174-99f4-487a-875d-7e19b5b5ea1b"
-FLOW_ID = ""
-APPLICATION_TOKEN = os.environ.get("APP_TOKEN")
+
+BASE_API_URL = "http://127.0.0.1:7860"
+FLOW_ID = "ccc25ce0-a56c-4903-a11d-1481e279d101"
+APPLICATION_TOKEN = os.environ.get("OPENAI_API_KEY")
 ENDPOINT = "PW05" # The endpoint name of the flow
 
 
 def run_flow(message: str) -> dict:
-    api_url = f"{BASE_API_URL}/lf/{LANGFLOW_ID}/api/v1/run/{ENDPOINT}"
+    api_url = f"{BASE_API_URL}/api/v1/run/{ENDPOINT}"
 
     payload = {
         "input_value": message,
@@ -22,7 +23,7 @@ def run_flow(message: str) -> dict:
         "input_type": "chat",
     }
 
-    headers = {"Authorization": "Bearer " + APPLICATION_TOKEN, "Content-Type": "application/json"}
+    headers = {"Authorization": f"Bearer {APPLICATION_TOKEN}", "Content-Type": "application/json"}
     response = requests.post(api_url, json=payload, headers=headers)
     return response.json()
 
